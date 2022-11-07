9e3f809c93306634cd7c9474638bed3ed43b26ce
Laura Caroline Ribeiro de Melo <laura@laura-melo.com>
2022-11-08 00:46:17 +0100

    Update color schema

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 const GRAY_BASE_HS = "274, 5%";
-const PRIMARY_BASE_HS = "274, 67%";
+const PRIMARY_BASE_HS = "126, 60%";
 const RED_BASE_HS = "0, 67%";
-const GREEN_BASE_HS = "126, 94%";
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -32,21 +31,13 @@ module.exports = {
       "white-primary": `hsl(${PRIMARY_BASE_HS}, 98%)`,
       "lightest-primary": `hsl(${PRIMARY_BASE_HS}, 95%)`,
       "extra-light-primary": `hsl(${PRIMARY_BASE_HS}, 85%)`,
-      "light-primary": `hsl(${PRIMARY_BASE_HS}, 69%)`,
-      "primary": `hsl(${PRIMARY_BASE_HS}, 53%)`,
-      "mid-primary": `hsl(${PRIMARY_BASE_HS}, 40%)`,
-      "dark-primary": `hsl(${PRIMARY_BASE_HS}, 28%)`,
-      "darkest-primary": `hsl(${PRIMARY_BASE_HS}, 15%)`,
-      "black-primary": `hsl(${PRIMARY_BASE_HS}, 8%)`,
+      "light-primary": `hsl(${PRIMARY_BASE_HS}, 60%)`,
+      "primary": `hsl(${PRIMARY_BASE_HS}, 40%)`,
+      "mid-primary": `hsl(${PRIMARY_BASE_HS}, 30%)`,
+      "dark-primary": `hsl(${PRIMARY_BASE_HS}, 20%)`,
+      "darkest-primary": `hsl(${PRIMARY_BASE_HS}, 10%)`,
+      "black-primary": `hsl(${PRIMARY_BASE_HS}, 5%)`,
 
-      "white-green": `hsl(${GREEN_BASE_HS}, 98%)`,
-      "lightest-green": `hsl(${GREEN_BASE_HS}, 95%)`,
-      "extra-light-green": `hsl(${GREEN_BASE_HS}, 85%)`,
-      "light-green": `hsl(${GREEN_BASE_HS}, 70%)`,
-      "green": `hsl(${GREEN_BASE_HS}, 57%)`,
-      "mid-green": `hsl(${GREEN_BASE_HS}, 36%)`,
-      "dark-green": `hsl(${GREEN_BASE_HS}, 28%)`,
-      "darkest-green": `hsl(${GREEN_BASE_HS}, 15%)`,
     },
     fontFamily: {
       montserrat: ["Montserrat", "ui-sans-serif", "system-ui"],

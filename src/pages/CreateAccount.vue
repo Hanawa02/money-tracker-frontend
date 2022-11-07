8ab9ce8c47a324f0afe47082477a2746a5b07544
Laura Caroline Ribeiro de Melo <laura@laura-melo.com>
2022-11-08 00:43:31 +0100

    Add create account page skeleton

---
+++ src/pages/CreateAccount.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="p-4 max-w-md mx-auto">
+    <div class="flex mb-4">
+      <label class="pr-2">Name:</label>
+      <input type="text" v-model="name" class="border-b w-full text-center" />
+    </div>
+
+    <div
+      v-if="errorMessage"
+      class="
+        bg-lightest-red
+        text-dark-red
+        rounded
+        p-3
+        mb-8
+        border border-light-red
+      "
+    >
+      {{ errorMessage }}
+    </div>
+    <button
+      @click="addAccount"
+      class="bg-red text-white px-4 py-3 rounded text-md font-medium w-full"
+    >
+      Add Account
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useMainStore } from "~/stores/main.store";
+import axios from "axios";
+import { useAxios } from "@vueuse/integrations/useAxios";
+import routePaths from "~/routes/paths";
+import { useRouter } from "vue-router";
+
+const mainStore = useMainStore();
+
+const name = ref<string>("");
+const eventDate = ref<Date>(new Date());
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+const router = useRouter();
+function goToHomePage() {
+  router.push(routePaths.homePage);
+}
+
+const errorMessage = ref("");
+
+async function addAccount() {
+  const payload = {
+    method: "POST",
+    data: {
+      name: name.value,
+    },
+  };
+
+  const { data, error } = await useAxios(`account`, payload, instance);
+
+  if (data.value && !error.value) {
+    mainStore.loadData();
+    goToHomePage();
+    return;
+  }
+
+  if (error) {
+    errorMessage.value =
+      error.value?.response?.data.error || error.value?.response?.data || "";
+  }
+}
+</script>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import CreateCost from "~/pages/CreateCost.vue";
 import CreatePayment from "~/pages/CreatePayment.vue";
+import CreateAccount from "~/pages/CreateAccount.vue";
 
 import Home from "~/pages/HomePage.vue";
 import paths from "~/routes/paths";
@@ -20,6 +21,11 @@ export const routes = {
     path: paths.createPaymentPage,
     component: CreatePayment,
     name: "createPayment",
+  },
+  createAccountPages: {
+    path: paths.createAccountPage,
+    component: CreateAccount,
+    name: "createAccount",
   },
 };
 

911d8f5c3aa1c92793dd609390fe5d0f0166e9ed
Laura Caroline Ribeiro de Melo <laura@laura-melo.com>
2022-11-29 00:15:42 +0100

    Add header and logout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="w-full min-h-screen h-full max-w-md mx-auto">
+    <header
+      v-if="isAuthenticated"
+      class="
+        w-full
+        border-b border-light-gray
+        sticky
+        top-0
+        flex
+        p-4
+        items-center
+      "
+    >
+      <span class="flex items-center w-full justify-center"
+        ><img src="/favicon.svg" class="w-6 h-6 flex-shrink-0 mr-2" /> Money
+        Tracker</span
+      >
+      <button class="self-flex-end underline text-mid-primary" @click="logout">
+        Logout
+      </button>
+    </header>
     <router-view v-slot="{ Component }">
       <transition name="fade">
         <component :is="Component" />
@@ -8,4 +28,15 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useAuthStore } from "./stores/auth.store";
+
+const authStore = useAuthStore();
+
+const isAuthenticated = computed(() => authStore.isAuthenticated);
+
+function logout() {
+  authStore.logout();
+}
+</script>

c80bf4db235a6a8c4a8b4865a53e0571aae60d30
Laura Caroline Ribeiro de Melo <laura@laura-melo.com>
2022-12-10 17:48:56 +0100

    Add Radio Tag component

---
+++ src/components/RadioTag.vue
@@ -0,0 +1,40 @@
+<template>
+  <label :for="id">
+    <m-tag
+      :tag="label"
+      :disabled="true"
+      class="justify-center"
+      :class="{
+        'bg-extra-light-primary font-medium': modelValue === value,
+      }"
+    />
+  </label>
+  <input
+    type="radio"
+    class="hidden"
+    v-model="modelValue"
+    :value="value"
+    :id="id"
+    @change="onChange"
+  />
+</template>
+
+<script setup lang="ts">
+import MTag from "~/components/MTag.vue";
+
+interface IProps {
+  id: string;
+  modelValue: string;
+  value: string;
+  label: string;
+}
+
+defineProps<IProps>();
+const emit =
+  defineEmits<{ (_event: "update:modelValue", _value: string): void }>();
+
+function onChange(event: Event) {
+  const newValue = (event.target as HTMLInputElement).value;
+  emit("update:modelValue", newValue);
+}
+</script>
